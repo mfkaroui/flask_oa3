e76800fe30f83119032e2bca23f1e36acde964bf
Mohamed Karoui <mfkaroui@gmail.com>
2023-11-20 09:11:29 -0500

    Added view docs test case

--- flask_oa3/tests/test_decorators.py
+++ flask_oa3/tests/test_decorators.py
@@ -1,5 +1,5 @@
 import pytest
-from ..decorators import specification_extensions_support
+from ..decorators import specification_extensions_support, view_docs
 from ..errors import ReservedSpecificationExtentionError
 
 class TestDecorators:
@@ -16,3 +16,23 @@ class TestDecorators:
         
         with pytest.raises(ReservedSpecificationExtentionError):
             test_function(oai_a="test")
+
+    def test_view_docs(self):
+        from ..view import View #Local import to not corrupt scope of test document, needed for the decorator
+        
+        summary = "Test summary"
+        description = "A long test description"
+
+        @view_docs(summary=summary, description=description)
+        class TestView(View):
+            def get(self, **kwargs):
+                pass
+        
+        assert "summary" in TestView.__api_docs__ and TestView.__api_docs__["summary"] == summary
+        assert "description" in TestView.__api_docs__ and TestView.__api_docs__["description"] == description
+
+        with pytest.raises(TypeError):
+            @view_docs(summary=summary, description=description)
+            class TestView:
+                def get(self, **kwargs):
+                    pass
