41a5aeab94f4ff2c0c57772947c9f94a6baf59cf
Mohamed Karoui <mfkaroui@gmail.com>
2023-11-20 00:11:53 -0500

    Added schema and support for options and head methods

--- flask_oa3/view.py
+++ flask_oa3/view.py
@@ -1,5 +1,5 @@
 import inspect
-from typing import Dict, List
+from typing import Dict, List, Union, Callable
 from .model import Model
 from .errors import ModelAlreadyRegisteredError
 
@@ -9,13 +9,28 @@ class View:
         "post",
         "put",
         "patch",
-        "delete"
+        "delete",
+        "options",
+        "head"
     ]
+    __api_docs__: Dict[str, str] = {}
 
     @classmethod
-    def _get_methods(cls):
+    def _get_methods(cls) -> Dict[str, Callable]:
         methods = {}
         for func_name, func in inspect.getmembers(cls, inspect.isfunction):
             if func_name.lower() in cls.ALLOWED_METHODS:
                 methods[func_name] = func
         return methods
+    
+    @classmethod
+    def schema(cls):
+        methods = cls._get_methods()
+        schema = {
+            method: {
+                "tags": [method],
+                "operationId": methods[method].__qualname__
+            } for method in methods
+        }
+        schema.update(cls.__api_docs__)
+        return schema
