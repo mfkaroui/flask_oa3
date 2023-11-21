b5c7e3db0440626dd55961d18424c658bd040782
Mohamed Karoui <mfkaroui@gmail.com>
2023-11-20 23:53:43 -0500

    Stubbed out supported field types

--- flask_oa3/fields.py
+++ flask_oa3/fields.py
@@ -1,1 +1,23 @@
-class Raw: ...
+class BaseField:
+    __FIELD_TYPE__ = None
+
+class NestedField(BaseField):
+    __FIELD_TYPE__ = "object"
+
+class ListField(BaseField):
+    __FIELD_TYPE__ = "list"
+
+class StringField(BaseField):
+    __FIELD_TYPE__ = "string"
+
+class BooleanField(BaseField):
+    __FIELD_TYPE__ = "boolean"
+
+class IntegerField(BaseField):
+    __FIELD_TYPE__ = "number"
+
+class FloatField(BaseField):
+    __FIELD_TYPE__ = "number"
+
+class ArbitraryField(BaseField):
+    __FIELD_TYPE__ = "number"
