0e9ce544faf1452b89f5b7c251ece72c5ee30565
Mohamed Karoui <mfkaroui@gmail.com>
2023-11-22 01:10:17 -0500

    Added field type unit tests

--- flask_oa3/tests/test_fields.py
+++ flask_oa3/tests/test_fields.py
@@ -1,3 +1,4 @@
+import pytest
 from ..fields import *
 
 class TestFields:
@@ -28,3 +29,24 @@ class TestFields:
     def test_arbitrary_field(self):
         field = ArbitraryField(description="This is a test", minimum=12)
         print("test")
+
+    @pytest.mark.parametrize(("field_class, expected_field_type"), [
+        (NestedField, FieldType.OBJECT),
+        (ListField, FieldType.ARRAY),
+        (StringField, FieldType.STRING),
+        (DateField, FieldType.STRING),
+        (DateTimeField, FieldType.STRING),
+        (PasswordField, FieldType.STRING),
+        (Base64EncodedField, FieldType.STRING),
+        (EmailField, FieldType.STRING),
+        (UUID4Field, FieldType.STRING),
+        (URIField, FieldType.STRING),
+        (IPv4Field, FieldType.STRING),
+        (IPv6Field, FieldType.STRING),
+        (BooleanField, FieldType.BOOLEAN),
+        (IntegerField, FieldType.INTEGER),
+        (FloatField, FieldType.NUMBER),
+        (ArbitraryField, FieldType.NUMBER)
+    ])
+    def test___FIELD_TYPE___value(self, field_class, expected_field_type):
+        assert field_class.__FIELD_TYPE__ == expected_field_type
