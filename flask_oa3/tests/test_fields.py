24ea820e3c1b3ebad3594ced50050edff5159f04
Mohamed Karoui <mfkaroui@gmail.com>
2023-11-22 00:04:23 -0500

    Stubbed out test cases

--- flask_oa3/tests/test_fields.py
+++ flask_oa3/tests/test_fields.py
@@ -1,6 +1,30 @@
-from ..fields import IntegerField
+from ..fields import *
 
 class TestFields:
+    def test_nested_field(self):
+        field = NestedField(description="This is a test", minimum=12)
+        print("test")
+    
+    def test_list_field(self):
+        field = ListField(description="This is a test", minimum=12)
+        print("test")
+
+    def test_string_field(self):
+        field = StringField(description="This is a test", minimum=12)
+        print("test")
+    
+    def test_boolean_field(self):
+        field = BooleanField(description="This is a test", minimum=12)
+        print("test")
+
     def test_integer_field(self):
         field = IntegerField(description="This is a test", minimum=12)
+        print("test")
+    
+    def test_float_field(self):
+        field = FloatField(description="This is a test", minimum=12)
+        print("test")
+    
+    def test_arbitrary_field(self):
+        field = ArbitraryField(description="This is a test", minimum=12)
         print("test")
