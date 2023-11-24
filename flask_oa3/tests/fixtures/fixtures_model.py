fbe0972cef252f1bc8e9a7d786ea37ecd8ceb50c
Mohamed Karoui <mfkaroui@gmail.com>
2023-11-24 10:15:55 -0500

    Added required fields to the model fixture

--- flask_oa3/tests/fixtures/fixtures_model.py
+++ flask_oa3/tests/fixtures/fixtures_model.py
@@ -6,11 +6,11 @@ from ...fields import *
 @pytest.fixture
 def model_fixture():
     class TestModel(Model):
-        id = IntegerField()
-        name = StringField()
-        weight = FloatField()
+        id = IntegerField(required=True)
+        name = StringField(required=True)
+        weight = FloatField(allow_null=True)
         is_active = BooleanField()
         created_at = DateTimeField()
         birthday = DateField()
-        email = EmailField()
+        email = EmailField(required=True)
     return TestModel
