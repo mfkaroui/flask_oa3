30b66fd8f5be86f437dfec027f0a6c74e35ecf61
Mohamed Karoui <mfkaroui@gmail.com>
2023-11-26 21:58:54 -0500

    Added schema property and support for examples

--- flask_oa3/media_types/media_type.py
+++ flask_oa3/media_types/media_type.py
@@ -17,3 +17,24 @@ class BaseMediaType:
         if cls.__MEDIA_TYPE__ is None:
             raise ValueError(f"The media type was not set")
         return cls.__MEDIA_TYPE__
+    
+    @property
+    def schema(self) -> dict:
+        """Constructs the Open API 'Media Type Object' according to specifications
+
+        Returns:
+            dict: The Open API schema
+        """     
+        schema = {
+            "schema": {
+                "$ref": self.model._get_component_name()
+            }
+        }
+        examples = self.model._get_examples()
+        if len(examples) > 0:
+            schema["examples"] = {
+                example_name: {
+                    "$ref": example._get_component_name()
+                } for example_name, example in examples
+            }
+        return schema
