a7787cd2adfde4fd98ad4df2bc96f7649a1c22cb
Mohamed Karoui <mfkaroui@gmail.com>
2023-11-28 09:11:30 -0500

    Added docstrings, added generate validator base on pydantic

--- flask_oa3/model.py
+++ flask_oa3/model.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 import inspect
 from typing import Dict, List
-
+from pydantic import BaseModel, create_model
 from .fields.metaclass import FieldBase
 
 class Model:
@@ -13,11 +13,43 @@ class Model:
 
     @classmethod
     def _get_fields(cls) -> Dict[str, FieldBase]:
+        """
+        Collects and returns a dictionary of field members belonging to the class and its base classes.
+
+        This method aggregates field members defined in the class and its base classes (if any are specified in `__EXTENDS__`).
+        It considers a member a field if it has an attribute `_meta` that equals `FieldBase`. The method traverses the class
+        hierarchy, gathering these fields into a dictionary, where keys are the field names and values are the corresponding
+        field instances.
+
+        Returns:
+            Dict[str, FieldBase]: A dictionary mapping field names to their respective field instances. These fields are
+            collected from the class and any classes specified in its `__EXTENDS__` attribute.
+        """
         fields = {}
+        if len(cls.__EXTENDS__) > 0:
+            for base in cls.__EXTENDS__:
+                fields.update(base._get_fields())
         for field_name, field in inspect.getmembers(cls, lambda m: hasattr(m, "_meta")):
             if field._meta == FieldBase:
                 fields[field_name] = field
         return fields
+
+    @classmethod
+    def generate_validator(cls) -> type[BaseModel]:
+        """
+        Dynamically generates a Pydantic model for input validation based on the class's defined fields.
+    
+        This method utilizes the `_get_fields` class method to retrieve all the relevant fields of the class.
+        It then constructs a Pydantic model where each field is transformed into a corresponding Pydantic field type,
+        enabling validation and parsing of input data according to the field specifications. The name of the generated
+        Pydantic model is derived from the class's name with 'Validator' appended.
+    
+        Returns:
+            type[BaseModel]: A dynamically created Pydantic model class, which can be used for validating
+            and parsing input data based on the field definitions of the current class.
+        """     
+        fields = {field_name: field._get_type() for field_name, field in cls._get_fields()}
+        return create_model(f"{cls.__name__}Validator", **fields)
 
     @classmethod
     def schema(cls):
