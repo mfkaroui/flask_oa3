56721f35f93b485f46443dbf66023f9f0254894b
Mohamed Karoui <mfkaroui@gmail.com>
2023-11-29 01:11:11 -0500

    removing old fields in favour of pydantic

--- flask_oa3/fields_old/boolean_fields.py
+++
@@ -1,9 +0,0 @@
-from .metaclass import FieldMeta
-from .mixin import RawMixin
-from .field_types import FieldType
-
-class BooleanField(RawMixin, metaclass=FieldMeta):
-    __FIELD_TYPE__ = FieldType.BOOLEAN
-
-    def __init__(self, **kwargs):
-        pass
