6f04670fa496f86d0d6781149b437b02e2e88072
Mohamed Karoui <mfkaroui@gmail.com>
2023-11-29 10:52:54 -0500

    Updated imports

--- flask_oa3/__init__.py
+++ flask_oa3/__init__.py
@@ -1,6 +1,12 @@
+from pydantic import Field
 from .licenses import Licenses
 from .api import API
+from .contact import Contact
 from .decorators import view_docs, tag
+from .discriminator import Discriminator
+from .external_documentation import ExternalDocumentation
+from .info import Info
 from .model import Model
 from .namespace import Namespace
+from .tag import Tag
 from .view import View
