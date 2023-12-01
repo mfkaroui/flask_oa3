516539b25caf899f699a153359811bffc6d2f5f2
Mohamed Karoui <mfkaroui@gmail.com>
2023-11-30 21:18:51 -0500

    Added imports

--- flask_oa3/__init__.py
+++ flask_oa3/__init__.py
@@ -4,6 +4,8 @@ from .api import API
 from .contact import Contact
 from .decorators import view_docs, tag
 from .discriminator import Discriminator
+from .encoding import Encoding
+from .example import Example
 from .external_documentation import ExternalDocumentation
 from .info import Info
 from .licenses import License
