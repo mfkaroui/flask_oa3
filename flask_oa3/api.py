518db0d2538ec12bc2c67152af31621508236b5c
Mohamed Karoui <mfkaroui@gmail.com>
2023-12-03 18:50:45 -0500

    Updated api to fall in line with design patterns

--- flask_oa3/api.py
+++ flask_oa3/api.py
@@ -9,26 +9,29 @@ from .namespace import Namespace
 from .view import View
 from .tag import Tag
 from .external_documentation import ExternalDocumentation
+from .components import Components
 
 class API(BaseModel):
     OPENAPI_VERSION: ClassVar[str] = "3.1.0"
     JSON_SCHEMA_DIALECT: ClassVar[AnyUrl] = "https://spec.openapis.org/oas/3.1/dialect/base"
-    api_provider: ClassVar[APIProvider] = APIProvider.FLASK
-    info: Annotated[Info, Field(description="REQUIRED. Provides metadata about the API. The metadata MAY be used by tooling as required.")]
-    tags: Annotated[Optional[List[Tag]], Field(default=[], description="A list of tags used by the document with additional metadata. The order of the tags can be used to reflect on their order by the parsing tools. Not all tags that are used by the Operation Object must be declared. The tags that are not declared MAY be organized randomly or based on the tools’ logic. Each tag name in the list MUST be unique.")] = []
+
     namespaces: ClassVar[Dict[str, Namespace]] = {}
     views: ClassVar[List[type[View]]] = []
-    external_documentation: Annotated[Optional[ExternalDocumentation], Field(alias="externalDocs", default=None, description="Additional external documentation.")]
-    
+
+
     @computed_field(alias="openapi", return_type=str, description="REQUIRED. This string MUST be the version number of the OpenAPI Specification that the OpenAPI document uses. The openapi field SHOULD be used by tooling to interpret the OpenAPI document. This is not related to the API info.version string.")
     @property
     def openapi_version(self) -> str:
         return self.OPENAPI_VERSION
-    
+    info: Annotated[Info, Field(description="REQUIRED. Provides metadata about the API. The metadata MAY be used by tooling as required.")]
     @computed_field(alias="jsonSchemaDialect", return_type=AnyUrl, description="REQUIRED. This string MUST be the version number of the OpenAPI Specification that the OpenAPI document uses. The openapi field SHOULD be used by tooling to interpret the OpenAPI document. This is not related to the API info.version string.")
     @property
     def json_schema_dialect(self) -> AnyUrl:
         return self.JSON_SCHEMA_DIALECT
+    components: Annotated[Optional[Components], Field(default=None, description="An element to hold various schemas for the document.")]
+    tags: Annotated[Optional[List[Tag]], Field(default=None, description="A list of tags used by the document with additional metadata. The order of the tags can be used to reflect on their order by the parsing tools. Not all tags that are used by the Operation Object must be declared. The tags that are not declared MAY be organized randomly or based on the tools’ logic. Each tag name in the list MUST be unique.")] = []
+    external_documentation: Annotated[Optional[ExternalDocumentation], Field(alias="externalDocs", default=None, description="Additional external documentation.")]
+    api_provider: Annotated[APIProvider, Field(default=APIProvider.FLASK, alias="x-api-provider", description="The provider that is serving the API")]
 
     def init_app(self) -> Flask:
         pass
@@ -37,6 +40,9 @@ class API(BaseModel):
         if namespace.name in self.namespaces:
             raise KeyError(f"A namespace with the name {namespace.name} is already registered")
         self.namespaces[namespace.name] = namespace
+        if self.tags is None:
+            self.tags = []
+        self.tags.append(self.namespaces[namespace.name]._get_tag())
 
     def register_view(self, view: View, route: str):
         full_route = self._parse_route(route)
@@ -54,20 +60,4 @@ class API(BaseModel):
         Returns:
             dict: The Open API schema
         """        
-        schema = {
-            "openapi": self.OPENAPI_VERSION,
-            "info": self.info.oa3_schema(),
-            "jsonSchemaDialect": "https://spec.openapis.org/oas/3.1/dialect/base",
-            "tags": {}
-        }
-        if self.tags is not None and len(self.tags) > 0:
-            schema["tags"].update({tag.name: tag.oa3_schema() for tag in self.tags})
-        if self.namespaces is not None and len(self.namespaces) > 0:
-            schema["tags"].update({namespace.name: namespace._get_tag().oa3_schema() for namespace in self.namespaces})
-        if len(schema["tags"]) == 0:
-            schema.pop("tags") #not used remove it
-        else:
-            schema["tags"] = list(schema["tags"].values()) #ensures that all tags have unique names
-        if self.external_documentation is not None:
-            schema["externalDocs"] = self.external_documentation.oa3_schema()
-        return schema
+        return self.model_dump(mode="json", by_alias=True, exclude_none=True)
