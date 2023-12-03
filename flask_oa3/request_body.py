bf413af08170bdada3d9510bf50e988f54cb8cb3
Mohamed Karoui <mfkaroui@gmail.com>
2023-12-03 18:31:00 -0500

    Added request body

---
+++ flask_oa3/request_body.py
@@ -0,0 +1,49 @@
+from typing import Optional, Annotated, Dict, ClassVar
+from enum import StrEnum
+from pydantic import BaseModel, Field, computed_field
+from .external_documentation import ExternalDocumentation
+from .component import Component, ComponentType
+from .media_types import MediaType
+
+class RequestBody(Component):
+    description: Annotated[Optional[str], Field(defualt=None, description="A brief description of the request body. This could contain examples of use. CommonMark syntax MAY be used for rich text representation.")]
+    required: Annotated[Optional[bool], Field(defualt=None, description="Determines if the request body is required in the request. Defaults to false.")]
+    _content: ClassVar[Dict[str, MediaType]] = {}
+
+    @computed_field(description="REQUIRED. The content of the request body. The key is a media type or media type range and the value describes it. For requests that match multiple keys, only the most specific key is applicable. e.g. text/plain overrides text/*")
+    @property
+    def content(self) -> Dict[str, MediaType]:
+        return self._content
+    
+    def add_media_type(self, media_type: MediaType):
+        """Adds a new media type to the content collection.
+
+        This method inserts the given `media_type` into the collection,
+        using its unique name as a key. It ensures that no duplicate media types
+        are added to the collection.
+
+        Args:
+            media_type (MediaType): The media type object to be added.
+
+        Raises:
+            TypeError: If the media_type argument is not an instance of MediaType
+            ValueError: If a media type with the same name already exists in the collection.
+        """        
+        if not isinstance(media_type, MediaType):
+            raise TypeError("Improper media type")
+        media_type_name = media_type._get_name()
+        if media_type_name in self._content:
+            raise ValueError("Media type already exists.")
+        self._content[media_type_name] = media_type
+
+    @property
+    def oa3_schema(self) -> dict:
+        """Constructs the Open API 'Request Body Object' according to specifications
+        
+        Spec:
+            https://spec.openapis.org/oas/v3.1.0#request-body-object
+        
+        Returns:
+            dict: The Open API schema
+        """        
+        return self.model_dump(mode="json", by_alias=True, exclude_none=True)
