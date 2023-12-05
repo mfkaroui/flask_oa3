adf50df450044e5cdb3c175340e3a9adab1a1bcd
Mohamed Karoui <mfkaroui@gmail.com>
2023-12-05 15:31:37 -0500

    Fixed imports

--- flask_oa3/__init__.py
+++ flask_oa3/__init__.py
@@ -1,15 +1,11 @@
 from pydantic import Field
 from .api_provider import APIProvider
-from .api import API
-from .contact import Contact
 from .decorators import view_docs, tag
-from .discriminator import Discriminator
-from .encoding import Encoding
-from .example import Example
-from .external_documentation import ExternalDocumentation
-from .info import Info
-from .licenses import License
 from .model import Model
 from .namespace import Namespace
-from .tag import Tag
 from .view import View
+
+import open_api_3 as oa3
+from .open_api_3 import license
+from .open_api_3 import response
+from .open_api_3 import media_type
