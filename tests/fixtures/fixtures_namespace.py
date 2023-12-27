14c7306cfb70c3c04df2ecacebc751f2110dcd2a
Mohamed Karoui <mfkaroui@gmail.com>
2023-12-27 12:16:52 -0500

    removing old tests

--- tests/fixtures/fixtures_namespace.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from ...namespace import Namespace
-
-@pytest.fixture
-def namespace_fixture() -> Namespace:
-    fixture = Namespace("test_namespace", "/test_namespace")
-    return fixture
--- tests/test_response.py
+++
@@ -1,5 +0,0 @@
-from ..responses import OKResponse
-
-class TestResponse:
-    def test__get_response_type(self):
-        OKResponse._get_response_type()
