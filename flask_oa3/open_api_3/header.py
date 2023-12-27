94373f68030090c099c5c985cec9c6c2141ec66f
Mohamed Karoui <mfkaroui@gmail.com>
2023-12-27 12:18:41 -0500

    whitespace

--- flask_oa3/open_api_3/header.py
+++ flask_oa3/open_api_3/header.py
@@ -8,7 +8,7 @@ class Header(Component):
     description: Annotated[Optional[str], Field(default=None, description="A brief description of the header. This could contain examples of use. CommonMark syntax MAY be used for rich text representation")]
     required: Annotated[Optional[bool], Field(default=None, description="Determines whether this header is mandatory. By default the behavior is false.")]
     deprecated: Annotated[Optional[bool], Field(default=None, description="Specifies that a header is deprecated and SHOULD be transitioned out of usage. Default value is false.")]
-        
+
     @property
     def oa3_schema(self) -> dict:
         """Constructs the Open API 'Header Object' according to specifications
