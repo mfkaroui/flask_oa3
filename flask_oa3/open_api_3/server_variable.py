2b6be4fc3c5e625d5997ea22d87d9668e0f1e9d5
Mohamed Karoui <mfkaroui@gmail.com>
2023-12-28 09:23:59 -0500

    Added model validator to server variable

--- flask_oa3/open_api_3/server_variable.py
+++ flask_oa3/open_api_3/server_variable.py
@@ -1,31 +1,19 @@
-from typing import Optional, Annotated, List
-from pydantic import BaseModel, Field, field_validator
+from typing import Optional, Annotated, List, Dict, Any
+from pydantic import BaseModel, Field, model_validator
+from .decorators import specification_extensions_support
 
+@specification_extensions_support
 class ServerVariable(BaseModel):
     enum: Annotated[Optional[List[str]], Field(default=None, description="An enumeration of string values to be used if the substitution options are from a limited set. The array MUST NOT be empty.")]
     default: Annotated[str, Field(description="REQUIRED. The default value to use for substitution, which SHALL be sent if an alternate value is not supplied. Note this behavior is different than the Schema Object’s treatment of default values, because in those cases parameter values are optional. If the enum is defined, the value MUST exist in the enum’s values.")]
     description: Annotated[Optional[str], Field(default=None, description="An optional description for the server variable. CommonMark syntax MAY be used for rich text representation.")]
 
-    @field_validator('enum')
-    def check_enum(cls, v):
-        if v is not None:
-            if not all(isinstance(item, str) and item for item in v):
-                raise ValueError('All items in enum must be non-empty strings.')
-        return v
-
-    @field_validator('default')
-    def check_default(cls, v, values, **kwargs):
-        if not v:
-            raise ValueError('The default value is required and cannot be empty.')
-        if 'enum' in values and v not in values['enum']:
-            raise ValueError('The default value must exist in the enum values.')
-        return v
-
-    @field_validator('description')
-    def check_description(cls, v):
-        if v is not None and not isinstance(v, str):
-            raise ValueError('The description must be a string.')
-        return v
+    @model_validator(mode="before")
+    @classmethod
+    def validate_default_enum(cls, values: Dict[str, Any]) -> Dict[str, Any]:
+        if ("enum" in values and values["enum"] is not None) and (values["default"] not in values["enum"]):
+            raise ValueError(f"Default value '{values['default']}' is not in enum: {values['enum']}")
+        return values
 
     @property
     def oa3_schema(self) -> dict:
