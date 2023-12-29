843962749eddd9f81a63f4828ae3af2ff8cd945c
Mohamed Karoui <mfkaroui@gmail.com>
2023-12-29 14:33:29 -0500

    removing bad argument

--- flask_oa3/open_api_3/responses.py
+++ flask_oa3/open_api_3/responses.py
@@ -12,7 +12,7 @@ class Responses(BaseModel):
     other_responses: Annotated[Optional[List[Union[Response, Reference[Response]]]], Field(description="Any HTTP status code can be used as the property name, but only one property per code, to describe the expected response for that HTTP status code. This field MUST be enclosed in quotation marks (for example, “200”) for compatibility between JSON and YAML. To define a range of response codes, this field MAY contain the uppercase wildcard character X. For example, 2XX represents all response codes between [200-299]. Only the following range definitions are allowed: 1XX, 2XX, 3XX, 4XX, and 5XX. If a response is defined using an explicit code, the explicit code definition takes precedence over the range definition for that code.")]
 
     @model_serializer(mode="plain", when_used="always")
-    def validate_status_codes(self, self: Responses) -> Dict[str, Union[Response, Reference[Response]]]:
+    def validate_status_codes(self) -> Dict[str, Union[Response, Reference[Response]]]:
         responses = {}
         if self.default is not None:
             if isinstance(response, Reference[Response]):
