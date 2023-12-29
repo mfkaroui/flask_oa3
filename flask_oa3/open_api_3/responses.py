93929b412211d76a25abd3ada682fd4554b4ad65
Mohamed Karoui <mfkaroui@gmail.com>
2023-12-28 23:15:57 -0500

    Added spec extension support and fixed documentation

--- flask_oa3/open_api_3/responses.py
+++ flask_oa3/open_api_3/responses.py
@@ -1,11 +1,14 @@
-from typing import Dict, Union, Annotated
+from typing import Dict, Union, Annotated, Optional
 from pydantic import RootModel, Field
 
 from .response import Response
 from .reference import Reference
+from .decorators import specification_extensions_support
 
+@specification_extensions_support
 class Responses(RootModel):
-    root: Annotated[Dict[str, Union[Response, Reference[Response]]], Field(description="The documentation of responses other than the ones declared for specific HTTP response codes. Use this field to cover undeclared responses.")]
+    root: Annotated[Dict[str, Union[Response, Reference[Response]]], Field(description="Any HTTP status code can be used as the property name, but only one property per code, to describe the expected response for that HTTP status code. This field MUST be enclosed in quotation marks (for example, “200”) for compatibility between JSON and YAML. To define a range of response codes, this field MAY contain the uppercase wildcard character X. For example, 2XX represents all response codes between [200-299]. Only the following range definitions are allowed: 1XX, 2XX, 3XX, 4XX, and 5XX. If a response is defined using an explicit code, the explicit code definition takes precedence over the range definition for that code.")]
+    default: Annotated[Optional[Union[Response, Reference[Response]]], Field(default=None, description="The documentation of responses other than the ones declared for specific HTTP response codes. Use this field to cover undeclared responses.")]
 
     @property
     def oa3_schema(self):
