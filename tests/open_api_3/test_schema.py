b6045b68d5ec4889ab55c16a8be0e7d9f27fcbd9
Mohamed Karoui <mfkaroui@gmail.com>
2024-01-03 12:15:24 -0500

    Added test cases for union

--- tests/open_api_3/test_schema.py
+++ tests/open_api_3/test_schema.py
@@ -60,6 +60,14 @@ class TestSchema:
         test_schema = Schema(schema_model=[reference_schema_fixture, schema_class_with_dict_fixture])
         assert test_schema.component_name == "all_of[ref.TestSchema, DictTestSchema]"
 
+    def test_schema_oa3_union_schema_component_name(self, schema_class_fixture, schema_class_with_dict_fixture):
+        test_schema = Schema(schema_model=(schema_class_fixture, schema_class_with_dict_fixture))
+        assert test_schema.component_name == "one_of[TestSchema, DictTestSchema]"
+
+    def test_schema_oa3_union_with_ref_schema_component_name(self, reference_schema_fixture, schema_class_with_dict_fixture):
+        test_schema = Schema(schema_model=(reference_schema_fixture, schema_class_with_dict_fixture))
+        assert test_schema.component_name == "one_of[ref.TestSchema, DictTestSchema]"
+
     def test_schema_specification_extensions(self, schema_with_specification_extensions_fixture):
         expected_schema: dict = {
             "properties": {
