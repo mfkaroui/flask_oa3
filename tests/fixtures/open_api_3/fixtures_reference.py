d9973b50c17fedff06c455a4a3044ea628faf890
Mohamed Karoui <mfkaroui@gmail.com>
2024-01-03 08:55:02 -0500

    stubbed out initial reference fixtures

---
+++ tests/fixtures/open_api_3/fixtures_reference.py
@@ -0,0 +1,12 @@
+import pytest
+from typing import Type, Optional
+from typing_extensions import Annotated
+from pydantic import BaseModel, Field
+
+from flask_oa3.open_api_3.reference import Reference
+from flask_oa3.open_api_3.schema import Schema
+
+@pytest.fixture
+def reference_schema_fixture(schema_class_fixture) -> Reference[Schema]:
+    test_schema = Schema(schema_model=schema_class_fixture)
+    yield Reference[Schema](component=test_schema)
