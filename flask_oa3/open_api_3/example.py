a24864c23c5cf41591674fbead72cfbca44dd98c
Mohamed Karoui <mfkaroui@gmail.com>
2024-01-05 08:33:44 -0500

    Fixing typo

--- flask_oa3/open_api_3/example.py
+++ flask_oa3/open_api_3/example.py
@@ -9,7 +9,7 @@ class Example(Component):
     summary: Annotated[Optional[str], Field(default=None, description="Short description for the example.")]
     description: Annotated[Optional[str], Field(default=None, description="Long description for the example. CommonMark syntax MAY be used for rich text representation.")]
     value: Annotated[Optional[Any], Field(default=None, description="Embedded literal example. The value field and externalValue field are mutually exclusive. To represent examples of media types that cannot naturally represented in JSON or YAML, use a string value to contain the example, escaping where necessary.")]
-    external_value: Annotated[Optional[AnyUrl], Field(default=None, alia="externalValue", description="A URI that points to the literal example. This provides the capability to reference examples that cannot easily be included in JSON or YAML documents. The value field and externalValue field are mutually exclusive. See the rules for resolving Relative References.")]
+    external_value: Annotated[Optional[AnyUrl], Field(default=None, alias="externalValue", description="A URI that points to the literal example. This provides the capability to reference examples that cannot easily be included in JSON or YAML documents. The value field and externalValue field are mutually exclusive. See the rules for resolving Relative References.")]
 
     @property
     def oa3_schema(self) -> dict:
